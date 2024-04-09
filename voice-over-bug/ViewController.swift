8f48524d442bd81bf88603acf2bc48ac77f92c1a
Potter Dai <potterdai@microsoft.com>
2024-04-09 09:38:08 -0700

    Initial Commit

---
+++ voice-over-bug/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  voice-over-bug
+//
+//  Created by potterdaims on 2024-04-09.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ voice-over-bugUITests/voice_over_bugUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  voice_over_bugUITestsLaunchTests.swift
+//  voice-over-bugUITests
+//
+//  Created by potterdaims on 2024-04-09.
+//
+
+import XCTest
+
+final class voice_over_bugUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
